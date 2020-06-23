be7640f1e123ed33fd47688573bc29be894c9c5c
ZHANG Zhen <zen@ZHANGs-MacBook-Pro.local>
2020-06-23 17:15:23 -0400

    Update utils.py

--- learner/utils.py
+++ learner/utils.py
@@ -47,12 +47,9 @@ def cross_entropy_loss(y_pred, y_label):
 def accuracy(x,y):
     max_x, argmax_x = torch.max(x, dim = 1)
     argmax_x = argmax_x.float()
-    print(argmax_x.dtype)
-    print(y.dtype)
-    print(argmax_x[:10])
-    print(y[:10])
-    print(argmax_x == y)
+    print(x.shape[0])
     print(torch.sum(argmax_x == y))
+    print(torch.sum(argmax_x == y)/x.shape[0])
     return torch.sum(argmax_x == y)/x.shape[0]
 
 def grad(y, x, create_graph=True, keepdim=False):
