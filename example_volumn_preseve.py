00492887d02b93e685ce97c4b8e3d33870cf217b
ZHANG Zhen <zen@ZHANGs-MacBook-Pro.local>
2020-06-24 15:37:02 -0400

    add callback

--- example_volumn_preseve.py
+++ example_volumn_preseve.py
@@ -6,10 +6,11 @@ Created on Tue Jun 23 22:34:17 2020
 @author: zen
 """
 
-from learner.utils import accuracy
 import learner as ln
 from torch.utils.data import DataLoader
 import torchvision
+import torch
+import os
 
 class MNISTData(ln.Data):
     def __init__(self):
@@ -26,7 +27,21 @@ class MNISTData(ln.Data):
         X_test = ts_loader.dataset.data[:64]
         self.X_test = X_test.view([X_test.shape[0],1,X_test.shape[1],X_test.shape[2]])*1.0/256
         self.y_test = ts_loader.dataset.targets[:64]
-
+        
+def callback(data, net):
+    x_orig = data.X_test
+    z = net(x_orig, reverse=False)
+    mask = torch.zeros(z.shape, device = z.device)
+    mask[:,:,0,:net.keep_dims] = 1
+    z = z*mask
+    x = net(z, reverse=True)
+    x = torch.sigmoid(x)
+    os.makedirs('samples_MNIST', exist_ok=True)
+    images_concat_orig = torchvision.utils.make_grid(x_orig, nrow=int(x_orig.shape[0] ** 0.5), padding=2, pad_value=255)
+    images_concat_recov = torchvision.utils.make_grid(x, nrow=int(x.shape[0] ** 0.5), padding=2, pad_value=255)
+    torchvision.utils.save_image(images_concat_orig, 'samples_MNIST/original.png')
+    torchvision.utils.save_image(images_concat_recov, 'samples_MNIST/recovered.png')
+    
 def main():
     device = 'gpu' # 'cpu' or 'gpu'
     data = MNISTData()
@@ -38,13 +53,14 @@ def main():
     outd = 392
     layers_c = 0
     layers_f = 4
+    keep_dims = 10
     # training
     lr = 0.01
     batch_size = 64
     iterations = 5000
     print_every = 10
     
-    net = ln.nn.VPNN(in_channels, hidden_channels, out_channels, ind, outd, layers_c, layers_f)
+    net = ln.nn.VPNN(in_channels, hidden_channels, out_channels, ind, outd, layers_c, layers_f, keep_dims)
     args = {
         'data': data,
         'net': net,
@@ -55,7 +71,7 @@ def main():
         'batch_size': batch_size,
         'print_every': print_every,
         'save': True,
-        'callback': None,
+        'callback': callback,
         'dtype': 'float',
         'device': device
     }
